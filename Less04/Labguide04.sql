1c92ce08f71964848ac334db929c446ada3e2d4d
unknown <phongwind155@gmail.com>
2023-12-16 23:20:57 +0700

    less04

---
+++ Less04/Labguide04.sql
@@ -0,0 +1,249 @@
+﻿--Bài 1: Viết những truy vấn lọc và sắp xếp dữ liệu 
+
+-- 1. Danh sách các môn học:
+SELECT MaMH, TenMH, Sotiet
+FROM MonHoc;
+-- 2. Danh sách sinh viên sắp xếp theo Mã sinh viên tăng dần:
+SELECT MaSV, HoSV, TenSV, HocBong
+FROM SinhVien
+ORDER BY MaSV ASC;
+-- 3. Danh sách sinh viên sắp xếp theo tên:
+SELECT MaSV, TenSV, Phai, NgaySinh
+FROM SinhVien
+ORDER BY TenSV;
+-- 4. Thông tin sinh viên sắp xếp theo Ngày sinh tăng dần và Học bổng giảm dần:
+SELECT HoSV + ' ' + TenSV AS HoTen, NgaySinh, HocBong
+FROM SinhVien
+ORDER BY NgaySinh ASC, HocBong DESC;
+-- 5. Danh sách các môn học có tên bắt đầu bằng chữ 'T':
+SELECT MaMH, TenMH, Sotiet
+FROM MonHoc
+WHERE TenMH LIKE 'T%';
+-- 6. Danh sách sinh viên có chữ cái cuối cùng trong tên là 'i':
+SELECT HoSV + ' ' + TenSV AS HoTen, NgaySinh, Phai
+FROM SinhVien
+WHERE SUBSTRING(TenSV, LEN(TenSV), 1) = 'i';
+--7. Danh sách khoa có ký tự thứ hai của tên khoa có chứa chữ 'N':
+SELECT MaKH, TenKH
+FROM Khoa
+WHERE SUBSTRING(TenKH, 2, 1) LIKE 'N%';
+--8. Danh sách sinh viên mà họ có chứa chữ 'Thị':
+SELECT HoSV + ' ' + TenSV AS HoTen, NgaySinh, Phai
+FROM SinhVien
+WHERE HoSV LIKE '%Thị%';
+--9. Danh sách sinh viên ký tự đầu tiên của tên nằm trong khoảng từ a đến m:
+SELECT MaSV, HoSV + ' ' + TenSV AS HoTen, Phai, HocBong
+FROM SinhVien
+WHERE ASCII(LEFT(TenSV, 1)) BETWEEN ASCII('a') AND ASCII('m');
+--10. Danh sách sinh viên tên chứa ký tự trong khoảng từ a đến m, sắp xếp tăng dần theo họ tên:
+SELECT HoSV + ' ' + TenSV AS HoTen, NgaySinh, NoiSinh, HocBong
+FROM SinhVien
+WHERE TenSV COLLATE SQL_Latin1_General_CP1_CI_AS BETWEEN 'a' AND 'm'
+ORDER BY HoTen;
+-- 11. Danh sách sinh viên của khoa Anh văn:
+SELECT MaSV, HoSV + ' ' + TenSV AS HoTen, NgaySinh, MaKH
+FROM SinhVien
+WHERE MaKH = 'AV';
+--12. Danh sách sinh viên của khoa Vật Lý, sắp xếp theo Ngày sinh giảm dần:
+SELECT MaSV, HoSV + ' ' + TenSV AS HoTen, NgaySinh
+FROM SinhVien
+WHERE MaKH = 'VL'
+ORDER BY NgaySinh DESC;
+--13. Danh sách sinh viên có học bổng lớn hơn 500,000, sắp xếp theo Mã khoa giảm dần:
+SELECT MaSV, HoSV + ' ' + TenSV AS HoTen, MaKH, HocBong
+FROM SinhVien
+WHERE HocBong > 500000
+ORDER BY MaKH DESC;
+--14. Danh sách sinh viên sinh vào ngày 20/12/1987:
+SELECT HoSV + ' ' + TenSV AS HoTen, MaKH, HocBong
+FROM SinhVien
+WHERE NgaySinh = '1987-12-20';
+--15. Danh sách sinh viên sinh sau ngày 20/12/1977, sắp xếp theo ngày sinh giảm dần:
+SELECT HoSV + ' ' + TenSV AS HoTen, NgaySinh, NoiSinh, HocBong
+FROM SinhVien
+WHERE NgaySinh > '1977-12-20'
+ORDER BY NgaySinh DESC;
+--16. Danh sách sinh viên có học bổng lớn hơn 100,000 và sinh ở Tp HCM:
+SELECT HoSV + ' ' + TenSV AS HoTen, MaKH, NoiSinh, HocBong
+FROM SinhVien
+WHERE HocBong > 100000 AND NoiSinh = 'Tp. HCM';
+--17. Danh sách sinh viên của khoa Anh văn và khoa Triết:
+SELECT MaSV, MaKH, Phai
+FROM SinhVien
+WHERE MaKH IN ('AV', 'TR');
+--18. Sinh viên có ngày sinh từ ngày 01/01/1986 đến ngày 05/06/1992:
+SELECT MaSV, NgaySinh, NoiSinh, HocBong
+FROM SinhVien
+WHERE NgaySinh BETWEEN '1986-01-01' AND '1992-06-05';
+--19. Sinh viên có học bổng từ 200,000 đến 800,000:
+SELECT MaSV, NgaySinh, Phai, MaKH
+FROM SinhVien
+WHERE HocBong BETWEEN 200000 AND 800000;
+--20. Môn học có số tiết lớn hơn 40 và nhỏ hơn 60:
+SELECT MaMH, TenMH, Sotiet
+FROM MonHoc
+WHERE Sotiet > 40 AND Sotiet < 60;
+--21. Sinh viên nam của khoa Anh văn:
+SELECT MaSV, HoSV + ' ' + TenSV AS HoTen, Phai
+FROM SinhVien
+WHERE Phai = 0 AND MaKH = 'AV';
+--22. Sinh viên có nơi sinh ở Hà Nội và ngày sinh sau ngày 01/01/1990:
+SELECT HoSV, TenSV, NoiSinh, NgaySinh
+FROM SinhVien
+WHERE NoiSinh = 'Hà Nội' AND NgaySinh > '1990-01-01';
+--23. Sinh viên nữ, tên có chứa chữ N:
+SELECT HoSV + ' ' + TenSV AS HoTen, Phai
+FROM SinhVien
+WHERE Phai = 1 AND (CHARINDEX('N', TenSV) > 0 OR CHARINDEX('n', TenSV) > 0);
+--24. Nam sinh viên khoa Tin Học có ngày sinh sau ngày 30/5/1986:
+SELECT MaSV, HoSV + ' ' + TenSV AS HoTen, NgaySinh
+FROM SinhVien
+WHERE Phai = 0 AND MaKH = 'TH' AND NgaySinh > '1986-05-30';
+--25. Danh sách sinh viên với giới tính (Nam/Nữ) và Ngày sinh:
+SELECT HoSV + ' ' + TenSV AS HoTen, CASE WHEN Phai = 0 THEN 'Nam' ELSE 'Nữ' END AS GioiTinh, NgaySinh
+FROM SinhVien;
+--26. Danh sách sinh viên với thông tin Mã sinh viên, Tuổi, Nơi sinh, Mã khoa:
+SELECT MaSV, YEAR(GETDATE()) - YEAR(NgaySinh) AS Tuoi, NoiSinh, MaKH
+FROM SinhVien;
+--27. Sinh viên có tuổi lớn hơn 20, thông tin gồm: Họ tên sinh viên, Tuổi, Học bổng:
+SELECT HoSV + ' ' + TenSV AS HoTen, YEAR(GETDATE()) - YEAR(NgaySinh) AS Tuoi, HocBong
+FROM SinhVien
+WHERE YEAR(GETDATE()) - YEAR(NgaySinh) > 20;
+--28. Danh sách sinh viên có tuổi từ 20 đến 30, thông tin gồm: Họ tên sinh viên, Tuổi, Tên khoa:
+SELECT HoSV + ' ' + TenSV AS HoTen, YEAR(GETDATE()) - YEAR(NgaySinh) AS Tuoi, TenKH
+FROM SinhVien
+JOIN Khoa ON SinhVien.MaKH = Khoa.MaKH
+WHERE YEAR(GETDATE()) - YEAR(NgaySinh) BETWEEN 20 AND 30;
+
+--=====================================================================================
+-- Truy vấn con  
+-- 1.
+SELECT MaSV, MaKH, Phai
+FROM SinhVien SV
+WHERE NOT EXISTS (SELECT 1 FROM Ketqua K WHERE SV.MaSV = K.MaSV);
+-- 2.
+SELECT SV.MaSV, SV.HoSV + ' ' + SV.TenSV AS HoTen, SV.MaKH
+FROM SinhVien SV
+WHERE NOT EXISTS (SELECT 1 FROM Ketqua K WHERE SV.MaSV = K.MaSV AND K.MaMH = '01');
+-- 3. 
+SELECT M.MaMH, M.TenMH, M.Sotiet
+FROM MonHoc M
+WHERE NOT EXISTS (SELECT 1 FROM Ketqua K WHERE M.MaMH = K.MaMH);
+-- 4.
+SELECT K.MaKH, K.TenKH
+FROM Khoa K
+WHERE NOT EXISTS (SELECT 1 FROM SinhVien SV WHERE K.MaKH = SV.MaKH);
+-- 5.
+SELECT SV.MaSV, SV.HoSV + ' ' + SV.TenSV AS HoTen, SV.MaKH
+FROM SinhVien SV
+WHERE SV.MaKH = 'AV' AND NOT EXISTS (SELECT 1 FROM Ketqua K WHERE SV.MaSV = K.MaSV AND K.MaMH = '01');
+-- 6.
+SELECT M.MaMH, M.TenMH, M.Sotiet
+FROM MonHoc M
+WHERE NOT EXISTS (SELECT 1 FROM Ketqua K JOIN SinhVien SV ON K.MaSV = SV.MaSV WHERE SV.MaKH = 'LY' AND M.MaMH = K.MaMH);
+-- 7.
+SELECT K1.MaSV, K1.MaMH, K1.Diem
+FROM Ketqua K1
+JOIN SinhVien SV ON K1.MaSV = SV.MaSV
+WHERE K1.MaMH = '04' AND K1.Diem < (SELECT MIN(K2.Diem) FROM Ketqua K2 JOIN SinhVien SV2 ON K2.MaSV = SV2.MaSV WHERE SV2.MaKH = 'TH' AND K2.MaMH = '04');
+-- 8.
+SELECT SV.MaSV, SV.HoSV + ' ' + SV.TenSV AS HoTen, SV.NgaySinh
+FROM SinhVien SV
+WHERE SV.NgaySinh > (SELECT MIN(NgaySinh) FROM SinhVien WHERE MaKH = 'AV');
+-- 9.
+SELECT SV.MaSV, SV.HocBong
+FROM SinhVien SV
+WHERE SV.HocBong > (SELECT SUM(HocBong) FROM SinhVien WHERE MaKH = 'TR');
+-- 10.
+SELECT SV.MaSV, SV.HoSV + ' ' + SV.TenSV AS HoTen, SV.NoiSinh
+FROM SinhVien SV
+WHERE SV.NoiSinh = (SELECT TOP 1 SV2.NoiSinh FROM SinhVien SV2 WHERE SV2.MaKH = 'LY' ORDER BY SV2.HocBong DESC);
+-- 11.
+SELECT K1.MaSV, SV.HoSV + ' ' + SV.TenSV AS HoTen, K1.MaMH, M.TenMH, K1.Diem
+FROM Ketqua K1
+JOIN SinhVien SV ON K1.MaSV = SV.MaSV
+JOIN MonHoc M ON K1.MaMH = M.MaMH
+WHERE K1.Diem = (SELECT MAX(K2.Diem) FROM Ketqua K2 WHERE K2.MaMH = K1.MaMH);
+-- 12.
+SELECT SV.MaSV, K.TenKH, SV.HocBong
+FROM SinhVien SV
+JOIN Khoa K ON SV.MaKH = K.MaKH
+WHERE SV.HocBong = (SELECT MAX(SV2.HocBong) FROM SinhVien SV2 WHERE SV2.MaKH = SV.MaKH);
+--=====================================================================================
+-- Thêm dữ liệu vào cơ sở dữ liệu 
+-- 1.
+INSERT INTO SinhVien (MaSV, HoSV, TenSV, Phai, NgaySinh, NoiSinh, MaKH, HocBong)
+VALUES ('C01', 'Lê Thành', 'Nguyên', 1, '1980-10-20', 'Thành phố Hồ Chí Minh', 'TH', 850000);
+-- 2. 
+INSERT INTO MonHoc (MaMH, TenMH, Sotiet)
+VALUES ('06', 'Xử lý ảnh', 45);
+-- 3. 
+INSERT INTO Khoa (MaKH, TenKH)
+VALUES ('CT', 'Công trình');
+-- 4.
+INSERT INTO SinhVien (MaSV, HoSV, TenSV, Phai, NgaySinh, NoiSinh, MaKH, HocBong)
+VALUES ('C02', 'Nguyễn Trần', 'Quân', 1, GETDATE(), 'Huế', 'CT', 950000);
+-- 5.
+INSERT INTO Ketqua (MaSV, MaMH, Diem)
+SELECT SV.MaSV, '06', 7
+FROM SinhVien SV
+INNER JOIN Khoa KH ON SV.MaKH = KH.MaKH
+WHERE KH.TenKH = 'Tin học';
+--6.
+INSERT INTO Ketqua (MaSV, MaMH, Diem)
+SELECT 'C02', MaMH, 8
+FROM MonHoc;
+--=====================================================================================
+-- Xoá thông tin trong cơ sở dữ liệu 
+-- 1.
+CREATE TABLE DeleteTable (
+    [MaSV] [nvarchar](3) NOT NULL,
+    [HoTen] [nvarchar](max) NOT NULL,
+    [Phai] [bit] NOT NULL,
+    [NgaySinh] [smalldatetime] NOT NULL,
+    [NoiSinh] [nvarchar](100) NOT NULL,
+    [TenKhoa] [nvarchar](50) NOT NULL,
+    [HocBong] [float] NULL
+);
+-- 2.
+DELETE FROM DeleteTable
+WHERE HocBong IS NULL;
+-- 3.
+DELETE FROM DeleteTable
+WHERE NgaySinh = '1987-12-20';
+-- 4.
+DELETE FROM DeleteTable
+WHERE NgaySinh < '1987-03-01';
+-- 5.
+DELETE FROM DeleteTable
+WHERE Phai = 1 AND TenKhoa = 'Tin học';
+--=====================================================================================
+-- Cập nhật thông tin trong cơ sở dữ liệu  
+-- 1.
+UPDATE MonHoc
+SET Sotiet = 45
+WHERE MaMH = 'Văn';
+-- 2.
+UPDATE SinhVien
+SET TenSV = 'Kỳ'
+WHERE HoSV = 'Trần Thanh' AND TenSV = 'Mai';
+-- 3.
+UPDATE SinhVien
+SET Phai = 1
+WHERE HoSV = 'Trần Thanh' AND TenSV = 'Kỳ';
+-- 4
+UPDATE SinhVien
+SET NgaySinh = '1990-07-05'
+WHERE HoSV = 'Trần Thị Thu' AND TenSV = 'Thuỷ';
+-- 5.
+UPDATE SinhVien
+SET HocBong = HocBong + 100000
+WHERE MaKH = 'AV';
+-- 6.
+UPDATE Ketqua
+SET Diem = CASE
+    WHEN Diem + 5 > 10 THEN 10
+    ELSE Diem + 5
+END
+WHERE MaMH = '02' AND MaSV IN (SELECT MaSV FROM SinhVien WHERE MaKH = 'AV');
+
---
+++ Less04/SQLQuery_Les04.sql
@@ -0,0 +1,287 @@
+﻿-- Less 04 
+
+-- 1. Truy vấn với select đơn giản (không có from)
+SELECT 'Devmaster'
+GO
+-- 2. Đặt tên cột cho dữ liệu trong tập kết quả 
+SELECT 'Devmaster' as [Company]
+GO
+SELECT 'Devmaster' as 'Company'
+GO
+SELECT 'Devmaster' [Company Name]
+GO
+SELECT 'Company' = 'Devmaster'
+-- 3. Truy vấn dữ liệu từ một bảng sử dụng dấu *
+-- Liệt kê tất cả dữ liệu trong bảng khoa
+-- (*)
+SELECT * 
+FROM KHOA
+GO
+-- 4. Lựa chọn các cột trong tập kết quả
+SELECT MaKH, TenKH
+FROM KHOA
+GO
+-- Liệt kê các thông tin masv, hosv, tensv, ngaysinh
+SELECT MASV, HOSV, TENSV, NGAYSINH
+FROM SINHVIEN
+GO
+-- 5. Sử dụng biểu thức trong select
+-- Liệt kê các thông tin masv, hosv, ngaysinh, trong đó cột hoten được gộp từ hosv, tensv
+SELECT MASV, HOSV + '' + TENSV as [Họ tên], NGAYSINH
+FROM SINHVIEN
+GO
+-- Liệt kê các thông tin điểm thi của sinh viên, thêm cột điểm tăng 10%
+SELECT * , [DIEM TANG] = DIEM*110/100
+FROM KETQUA
+-- 6. Sử dụng mệnh đề where để giới hạn dữ liệu trong tập kết quả (lọc, tìm kiếm)
+-- WHERE <Biểu thức điều kiện>
+/*
+	- Điều kiện sử dụng các phép toán so sánh
+		(>; >=; <=; =; <>; !=) => thường dùng với cột dữ liệu kiểu số
+	- Điều kiện sử dụng các phép toán tương đối (áp dụng với dữ liệu kiểu chuỗi)
+		+ Like / Not like:
+			+++ các ký tự đại diện:
+				++: dấu % đại diện cho chuỗi ký tự bất kỳ có thể rỗng
+				++: dấu _ đại diện cho 1 ký tự bất kỳ
+				++: [a-m]: khoảng ký tự từ a đến m
+	- Sử dụng toán tử quan hệ (and, or, not) trong biểu thức điều kiện
+	- Điều kiện so sánh tập hợp: IN / NOT IN
+
+*/
+
+/*
+	SELECT
+	FROM
+	WHERE
+*/
+-- Liệt kê sinh viên có học bổng lớn hơn hoặc bằng 100000
+SELECT *
+FROM SINHVIEN 
+WHERE HOCBONG >= 100000
+GO
+--Liệt kê sinh viên có học bổng trong khoảng 10.000 đến 100.000
+SELECT *
+FROM SINHVIEN 
+WHERE HOCBONG >= 10000 AND HOCBONG <= 100000
+GO
+--
+SELECT *
+FROM SINHVIEN 
+WHERE HOCBONG between 10000 AND 100000
+GO
+--- so sánh tương đối ( LIKE / NOT LIKE)
+-- Liệt kê sinh viên có tên bắt đầu bằng 'T'
+SELECT *
+FROM SINHVIEN 
+WHERE TENSV LIKE N'T%'
+GO
+-- Liệt kê sinh viên có tên chứa kí tự 'u'
+SELECT *
+FROM SINHVIEN 
+WHERE TENSV LIKE N'%u%'
+GO
+-- Liệt kê sinh viên có tên mà ký tự thứ 2 là 'u'
+SELECT *
+FROM SINHVIEN 
+WHERE TENSV LIKE N'_u%'
+GO
+-- Liệt kê sinh viên có tên chứa 3 kí tự
+SELECT *
+FROM SINHVIEN 
+WHERE TENSV LIKE N'___'
+GO
+--
+SELECT *
+FROM SINHVIEN 
+WHERE len(TENSV) = 3
+GO
+-- Liệt kê sinh viên có tên có chứa ký tự đầu tiên trong khoảng từ a-m
+SELECT *
+FROM SINHVIEN 
+WHERE TENSV LIKE N'[a-m]%'
+GO
+-- Không chứa
+SELECT *
+FROM SINHVIEN 
+WHERE TENSV NOT LIKE N'[a-m]%'
+GO
+--
+SELECT *
+FROM SINHVIEN 
+WHERE TENSV LIKE N'[^a-m]%'
+GO
+-- Sử dụng tập hợp 
+-- Liệt kê sinh viên có tháng sinh là tháng 2 và tháng 8
+SELECT [MaSV], [HoSV], [TenSV], [Phai], [NgaySinh], [NoiSinh], [MaKH], [HocBong], [DiemTrungBinh]
+FROM SINHVIEN
+WHERE MONTH(NGAYSINH) = 2 OR MONTH(NGAYSINH) = 8
+GO
+SELECT [MaSV], [HoSV], [TenSV], [Phai], [NgaySinh], [NoiSinh], [MaKH], [HocBong], [DiemTrungBinh]
+FROM SINHVIEN
+WHERE MONTH(NGAYSINH) IN (2,8)
+GO
+--================================
+-- 7. Sắp xếp dữ liệu trong tập kết quả
+-- sắp tăng: ORDER BY <column_Name> | <index> | <alise name> ASC (Mặc định)
+-- sắp giảm: ORDER BY DESC
+-- Liệt kê sinh viên, dữ liệu sắp theo tên sinh viên tăng dần
+SELECT  MASV, HOSV AS [HỌ SINH VIÊN], TENSV AS [TÊN SINH VIÊN]
+FROM SINHVIEN
+ORDER BY TENSV ASC
+--
+SELECT  MASV, HOSV AS [HỌ SINH VIÊN], TENSV AS [TÊN SINH VIÊN]
+FROM SINHVIEN
+ORDER BY 3 ASC
+--
+SELECT  MASV, HOSV AS [HỌ SINH VIÊN], TENSV AS [TÊN SINH VIÊN]
+FROM SINHVIEN
+ORDER BY [TÊN SINH VIÊN] ASC
+-- DESC
+SELECT  MASV, HOSV AS [HỌ SINH VIÊN], TENSV AS [TÊN SINH VIÊN]
+FROM SINHVIEN
+ORDER BY TENSV DESC
+-- 
+
+
+-- Luyện tập
+-- 1. Cho biết danh sách các môn học, gồm các thông tin sau: Mã môn học, Tên môn học, Số tiết.
+SELECT  * 
+FROM MONHOC
+-- 2.Liệt kê danh sách sinh viên, gồm các thông tin sau: Mã sinh viên, Họ sinh viên, Tên sinh viên, Học bổng. 
+--Danh sách sẽ được sắp xếp theo thứ tự Mã sinh viên tăng dần.
+SELECT  MASV, HOSV, TENSV, HocBong 
+FROM SINHVIEN
+ORDER BY MaSV ASC
+--3. Danh sách các sinh viên, gồm các thông tin sau: Mã sinh viên, Tên sinh viên, Phái, Ngày sinh. 
+--Danh sách sẽ được sắp xếp theo thứ tự của tên.
+SELECT  MASV, HOSV, TENSV, HocBong 
+FROM SINHVIEN
+ORDER BY TenSV ASC
+-- 4. Thông tin các sinh viên gồm: Họ tên sinh viên, Ngày sinh, Học bổng. 
+-- Thông tin sẽ được sắp xếp theo thứ tự Ngày sinh tăng dần và Học bổng giảm dần.
+
+
+/*
+15. Cho biết các sinh viên sinh sau ngày 20/12/1977, gồm các thông tin: Họ tên
+sinh viên, Ngày sinh, Nơi sinh, Học bổng. Danh sách sẽ được sắp xếp theo
+thứ tự ngày sinh giảm dần.
+*/
+
+set dateformat dmy
+go
+SELECT  HOSV, TENSV, CONVERT(VARCHAR(10),NGAYSINH,103) AS 'NGAYSINH',NOISINH, HocBong 
+FROM SINHVIEN
+WHERE NgaySinh > '12/20/1977'
+ORDER BY NgaySinh DESC
+
+
+--====================
+-- 7. sử dụng từ khóa TOP trong select
+-- TOP: trích ra số bản ghi đầu tiên trong tập kết quả
+SELECT *
+FROM SINHVIEN
+GO
+SELECT TOP 1*
+FROM SINHVIEN
+GO
+SELECT TOP 5*
+FROM SINHVIEN
+GO
+-- TOP 50%
+SELECT TOP 50 PERCENT *
+FROM SINHVIEN
+GO
+-- Lấy ra sinh viên có học bổng cao nhất
+SELECT TOP 1*
+FROM SINHVIEN
+ORDER BY HocBong DESC
+GO
+-- Lấy ra những sinh viên có học bổng cao nhất
+SELECT TOP 1 WITH TIES*
+FROM SINHVIEN
+ORDER BY HocBong DESC
+GO
+
+--===================
+-- 8. Sử dụng từ khóa DISTINCT TRONG SELEC
+-- DISTINCT: Loại bỏ dòng trùng lặp (giữ lại 1)
+SELECT * 
+FROM SINHVIEN
+GO -- các bản ghi là duy nhất (PK)
+
+SELECT MaKH
+FROM SINHVIEN
+GO -- lặp -> 15 bản ghi
+
+SELECT distinct MaKH
+FROM SINHVIEN
+GO
+
+--===========================
+-- 9. case ... when ... then trong select
+--: Liệt kê sinh viên, trong đó phái hiển thị là: 0 ~ Nam; 1 ~ Nữ; null ~ Khác
+SELECT [MaSV], [HoSV], [TenSV], [Phai] = CASE PHAI
+											WHEN 0 THEN N'NAM'
+											WHEN 1 THEN N'NỮ'
+											ELSE N'KHÁC'
+										END
+, [NgaySinh], [NoiSinh], [MaKH], [HocBong], [DiemTrungBinh]
+FROM SINHVIEN
+GO
+--========================================================
+--10. Sử dụng hàm IIF => IIF( boolean_expression, true_value, false_value)
+SELECT [MaSV], [HoSV], [TenSV], [Phai] = IIF (PHAI =0, N'NAM', IIF (PHAI =1,N'NỮ', N'KHÁC'))									
+, [NgaySinh], [NoiSinh], [MaKH], [HocBong], [DiemTrungBinh]
+FROM SINHVIEN
+GO
+--========================================================
+--11. Truy vấn kết hợp 2 hay nhiều bảng (JOIN)
+/*
+	INNER JOIN
+	LEFT JOIN / LEFT OUTER JOIN
+	RIGHT JOIN / RIGHT OUTER JOIN
+	FULL JOIN / FULL OUTER JOIN
+*/
+-- INNER JOIN:
+SELECT *
+FROM KHOA AS K
+	JOIN SINHVIEN S ON K.MaKH = S.MaKH
+GO
+SELECT *
+FROM KHOA AS K, SINHVIEN AS S
+WHERE K.MaKH = S.MaKH
+GO -- SO SÁNH
+-- LEFT JOIN:
+SELECT *
+FROM KHOA AS K
+	LEFT JOIN SINHVIEN S ON K.MaKH = S.MaKH
+GO
+-- RIGHT JOIN:
+SELECT *
+FROM KHOA AS K
+	RIGHT JOIN SINHVIEN S ON K.MaKH = S.MaKH
+GO
+-- FULL JOIN:
+SELECT *
+FROM KHOA AS K
+	FULL JOIN SINHVIEN S ON K.MaKH = S.MaKH
+GO
+-- Liệt kê các thông tin MAKH, TENKH, HOTEN, NGAYSINH, PHAI, HOCBONG
+SELECT * 
+FROM KHOA AS K
+	JOIN SINHVIEN AS S ON K.MaKH= S.MaKH
+GO
+-- Liệt kê các thông tin: MAKH, TENKH, HOSV, TENSV, PHAI, DIEM
+-- KHOA, SINHVIEN, KETQUA
+SELECT K.MAKH, S.MASV, TENKH, HOSV, TENSV, PHAI, DIEM
+FROM KHOA AS K
+	JOIN SINHVIEN AS S ON K.MaKH = S.MaKH
+	JOIN KETQUA AS Q ON S.MASV = Q.MASV
+GO
+-- DESIGNER
+
+
+--- Luyện tập lab04
+-- Nghiên cứu truy vấn gộp nhóm / điều kiện gộp nhóm
+-- truy vấn con
+-- truy vấn tham chiếu
